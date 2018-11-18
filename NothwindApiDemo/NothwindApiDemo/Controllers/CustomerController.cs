e47b65c236e2e177b78b663a3da318b32b01a972
Cesar Llanes <cllamed@hotmail.com>
2018-11-17 20:02:01 -0600

    Primer retorno de datos sin conexion a BD

---
+++ NothwindApiDemo/NothwindApiDemo/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NothwindApiDemo.Controllers
+{
+    public class CustomerController: Controller
+    {
+        public JsonResult GetCustomers()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { CustomerID = 1, ContactName = "Anderson"},
+                new { CustomerID = 2, ContactName = "Solaris"}
+            });
+        }
+
+    }
+}
