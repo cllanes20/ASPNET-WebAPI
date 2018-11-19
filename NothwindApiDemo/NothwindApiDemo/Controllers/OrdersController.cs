5e91c2229a437e95fe56d5fb5d0f54e66a1442f2
Cesar Llanes <cllamed@hotmail.com>
2018-11-19 12:58:42 -0600

    creando un nuevo registro

--- NothwindApiDemo/NothwindApiDemo/Controllers/OrdersController.cs
+++ NothwindApiDemo/NothwindApiDemo/Controllers/OrdersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NothwindApiDemo.Models;
 
 namespace NothwindApiDemo.Controllers
 {
@@ -22,7 +23,7 @@ namespace NothwindApiDemo.Controllers
             return Ok(customer.Orders);
         }
 
-        [HttpGet("{customerId}/orders/{id}")]
+        [HttpGet("{customerId}/orders/{id}", Name = "GetOrder")]
         public IActionResult GetOrder(int customerId, int id)
         {
             var customer = Repository.Instance.Customers.FirstOrDefault(c => c.Id == customerId);
@@ -38,5 +39,43 @@ namespace NothwindApiDemo.Controllers
             }
             return Ok(order);
         }
+
+        [HttpPost("{customerId}/orders")]
+        public IActionResult CreateOrder(int customerId, [FromBody] OrdersForCreationDTO order)
+        {
+            if (order == null)
+            {
+                return BadRequest();
+            }
+            var customer = Repository.Instance.Customers.FirstOrDefault(c => c.Id == customerId);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            var maxOrderId = Repository.Instance.Customers.SelectMany(c => c.Orders).Max(o => o.OrderId);
+
+            var finalOrder = new OrdersDTO()
+            {
+                OrderId = maxOrderId++,
+                CustomerId = order.CustomerId,
+                EmployeeId = order.EmployeeId,
+                OrderDate = order.OrderDate,
+                RequiredDate = order.RequiredDate,
+                ShippedDate = order.ShippedDate,
+                ShipVia = order.ShipVia,
+                Freight = order.Freight,
+                ShipName = order.ShipName,
+                ShipAddress = order.ShipAddress,
+                ShipCity = order.ShipCity,
+                ShipRegion = order.ShipRegion,
+                ShipPostalCode = order.ShipPostalCode,
+                ShipCountry = order.ShipCountry
+            };
+            customer.Orders.Add(finalOrder);
+
+            return CreatedAtRoute("GetOrder", new {customerId = customerId, id = finalOrder.OrderId }, finalOrder);
+        }
+
     }
 }
